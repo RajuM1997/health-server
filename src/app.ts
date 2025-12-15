6ed632f20ac6c9b70400c5a6ba7d7f875ed9ff77
RajuM1997 <mraju5797@gmail.com>
2025-12-16 00:16:56 +0600

    project set-up and some route added

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,34 @@
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import express, { Application, NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
-import cron from 'node-cron';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { AppointmentService } from './app/modules/Appointment/appointment.service';
-import { PaymentController } from './app/modules/Payment/payment.controller';
-import router from './app/routes';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import config from "./config";
+import router from "./app/routes";
 
 const app: Application = express();
-app.use(cookieParser());
-
-app.post(
-    "/webhook",
-    express.raw({ type: "application/json" }),
-    PaymentController.handleStripeWebhookEvent
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
 );
-
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    credentials: true
-}));
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", router);
 
-
-cron.schedule('*/5 * * * *', () => {
-    try {
-        console.log("🔄 Running unpaid appointment cleanup at", new Date().toISOString());
-        AppointmentService.cancelUnpaidAppointments();
-    } catch (err) {
-        console.error("❌ Cron job error:", err);
-    }
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Server is running..",
+    environment: config.node_env,
+    uptime: process.uptime().toFixed(2) + " sec",
+    timeStamp: new Date().toISOString(),
+  });
 });
-
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Ph health care server.."
-    })
-});
-
-app.use('/api/v1', router);
 
 app.use(globalErrorHandler);
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(httpStatus.NOT_FOUND).json({
-        success: false,
-        message: "API NOT FOUND!",
-        error: {
-            path: req.originalUrl,
-            message: "Your requested path is not found!"
-        }
-    })
-})
+app.use(notFound);
 
 export default app;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,80 +1,15 @@
-import express from 'express';
-import { apiLimiter } from '../middlewares/rateLimiter';
-import { AdminRoutes } from '../modules/Admin/admin.routes';
-import { AppointmentRoutes } from '../modules/Appointment/appointment.routes';
-import { AuthRoutes } from '../modules/Auth/auth.routes';
-import { DoctorRoutes } from '../modules/Doctor/doctor.routes';
-import { DoctorScheduleRoutes } from '../modules/DoctorSchedule/doctorSchedule.routes';
-import { MetaRoutes } from '../modules/Meta/meta.routes';
-import { PatientRoutes } from '../modules/Patient/patient.route';
-import { PaymentRoutes } from '../modules/Payment/payment.routes';
-import { PrescriptionRoutes } from '../modules/Prescription/prescription.routes';
-import { ReviewRoutes } from '../modules/Review/review.routes';
-import { ScheduleRoutes } from '../modules/Schedule/schedule.routes';
-import { SpecialtiesRoutes } from '../modules/Specialties/specialties.routes';
-import { userRoutes } from '../modules/User/user.routes';
+import express from "express";
+import { userRoutes } from "../modules/user/user.routes";
 
 const router = express.Router();
 
-
-
-router.use(apiLimiter); // Apply to all routes
-
 const moduleRoutes = [
-    {
-        path: '/user',
-        route: userRoutes
-    },
-    {
-        path: '/admin',
-        route: AdminRoutes
-    },
-    {
-        path: '/auth',
-        route: AuthRoutes
-    },
-    {
-        path: '/specialties',
-        route: SpecialtiesRoutes
-    },
-    {
-        path: '/doctor',
-        route: DoctorRoutes
-    },
-    {
-        path: '/patient',
-        route: PatientRoutes
-    },
-    {
-        path: '/schedule',
-        route: ScheduleRoutes
-    },
-    {
-        path: '/doctor-schedule',
-        route: DoctorScheduleRoutes
-    },
-    {
-        path: '/appointment',
-        route: AppointmentRoutes
-    },
-    {
-        path: '/payment',
-        route: PaymentRoutes
-    },
-    {
-        path: '/prescription',
-        route: PrescriptionRoutes
-    },
-    {
-        path: '/review',
-        route: ReviewRoutes
-    },
-    {
-        path: '/meta',
-        route: MetaRoutes
-    }
+  {
+    path: "/user",
+    route: userRoutes,
+  },
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route))
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
